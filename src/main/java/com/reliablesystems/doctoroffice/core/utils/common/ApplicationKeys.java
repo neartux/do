8bc61863725a19ddae106785b34f3d0f3020422e
rdzul <near31_3112@hotmail.com>
2018-07-17 15:36:34 -0500

    changes to view data patient

--- src/main/java/com/reliablesystems/doctoroffice/core/utils/common/ApplicationKeys.java
+++ src/main/java/com/reliablesystems/doctoroffice/core/utils/common/ApplicationKeys.java
@@ -3,4 +3,7 @@ package com.reliablesystems.doctoroffice.core.utils.common;
 public final class ApplicationKeys {
 
     private ApplicationKeys() {}
+
+    public static final String SEX_FEMALE = "FEMALE";
+    public static final String SEX_MALE = "MALE";
 }
